d4e994b77a53e97b9a1fa332e5233df41db30992
Tania <taniavasquez072006@gmail.com>
2025-12-01 09:22:05 -0600

    ruta manuales

--- views/Manuales/principal.php
+++ views/Manuales/principal.php
@@ -1,7 +1,7 @@
 <?php 
 @session_start();
 $rol= $_SESSION['idrol'];
-$ruta=($rol == 1)? 'Admin/Administrador.pdf':'docente/Usuario.pdf';
+$ruta=($rol == 1)? 'Admin/Administrador.pdf':'docente/Docente.pdf';
 ?>
 <div class="container-fluid">
     <embed src="./views/Manuales/<?= $ruta; ?> ?>" type="application/pdf" width="100%" height="500px">
